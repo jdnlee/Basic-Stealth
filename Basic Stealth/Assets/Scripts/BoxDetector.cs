df53bf0dc1646503f0314c55f849fccac2f9dd65
Shroomy <jaydenlee458@gmail.com>
2019-04-16 16:55:43 +1000

    Upload

---
+++ Basic Stealth/Assets/Scripts/BoxDetector.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BoxDetector : MonoBehaviour
+{
+ void OnTriggerEnter (Collider other)
+    {
+        if (other.gameObject.name == "Character")
+        {
+            Debug.Log("Found You!");
+        }
+    }
+}
---
+++ Basic Stealth/Assets/Scripts/Movement.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
+    
+    
+    void Update ()
+    {
+        if(Input.GetKey(KeyCode.W))
+            transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.S))
+            transform.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.A))
+            transform.Rotate(Vector3.up, -turnSpeed * Time.deltaTime);
+        
+        if(Input.GetKey(KeyCode.D))
+            transform.Rotate(Vector3.up, turnSpeed * Time.deltaTime);
+    }
+}
