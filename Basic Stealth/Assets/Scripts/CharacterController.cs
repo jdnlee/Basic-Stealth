14ae5b276b91660eaa6d7a52f6300059e195cbea
Shroomy <jaydenlee458@gmail.com>
2019-04-17 14:37:59 +1000

    NavMesh and Movement
    Added a NavMesh and a NavMEsh Controller. Allows the cylinder to move to wherever the mouse is located when it is clicked. The cylinder moves around other objects  and avoids running into them.

---
+++ Basic Stealth/Assets/Scripts/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+public class CharacterController : MonoBehaviour
+{
+
+    public Camera cam;
+    public NavMeshAgent  agent;
+
+    // Update is called once per frame
+    void Update()
+    {   
+        // CHECK IF THE MOUSE IS PRESSED
+        if (Input.GetMouseButtonDown(0))
+        {
+            // SENDING OUT A RAY TO WHERE THE MOUSE IS LOCATED
+           Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+           RaycastHit hit; 
+
+            // CHECKING IF THE RAY HITS SOMETHING 
+           if (Physics.Raycast(ray, out hit))
+           {
+               // MOVE OUR AGENT
+               agent.SetDestination(hit.point);
+           }
+
+        }
+    }
+}
